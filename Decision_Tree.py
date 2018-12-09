2e649ac37e3c921d7e346f0a5af764e3aca931a6
Your Name <email@example.com>
2018-12-09 12:51:43 +0800

    09/12

--- Decision_Tree.py
+++ Decision_Tree.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 from math import log
-
+import operator
 """
 函数说明:创建测试数据集
 
@@ -28,7 +28,7 @@ def createDataSet():
         [2, 1, 0, 2, 'yes'],
         [2, 0, 0, 0, 'no']
     ]
-    labels = ['不放贷','放贷']
+    labels = ['年龄', '有工作', '有自己的房子', '信贷情况']
     return dataset, labels
 
 """
@@ -127,8 +127,64 @@ def chooseBestFeatureToSplit(dataSet):
             bestFeature = i
     #返回信息增益最大的特征的索引值
     return bestFeature
-        
 
+"""
+函数说明:统计classList中出现此处最多的元素(类标签)
+
+Parameters:
+    classList - 类标签列表
+Returns:
+    sortedClassCount[0][0] - 出现此处最多的元素(类标签)
+"""
+def majorityCnt(classList):
+    classCount = {}
+    #统计classList中每个元素出现的次数
+    for vote in classList:
+        if vote not in classCount.keys():classCount[vote] = 0
+        classCount[vote] += 1
+    #根据字典的值降序排序
+    sortedClassCount = sorted(classCount.items(),key = operator.itemgetter(1),reverse = True)
+    #返回classList中出现次数最多的元素
+    return sortedClassCount[0][0]
+
+"""
+函数说明:创建决策树
+ 
+Parameters:
+    dataSet - 训练数据集
+    labels - 分类属性标签
+    featLabels - 存储选择的最优特征标签
+Returns:
+    myTree - 决策树
+"""
+def createTree(dataSet, labels, featLabels):
+    #取分类标签(是否放贷:yes or no)
+    classList = [example[-1] for example in dataSet]
+    #如果类别完全相同则停止继续划分
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    #遍历完所有特征时返回出现次数最多的类标签
+    if len(dataSet[0]) == 1 or len(labels) == 0:
+        return majorityCnt(classList)
+    #选择最优特征
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    #最优特征的标签
+    bestFeatLabel = labels[bestFeat]
+    featLabels.append(bestFeatLabel)
+    #根据最优特征的标签生成树
+    myTree = {bestFeatLabel:{}}
+    #删除已经使用特征标签
+    del(labels[bestFeat])
+    #得到训练集中所有最优特征的属性值
+    featValues = [example[bestFeat] for example in dataSet]
+    #去掉重复的属性值
+    uniqueVals = set(featValues)
+    #遍历特征，创建决策树
+    for value in uniqueVals:
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), labels, featLabels)
+    return myTree
 if __name__ == '__main__':
-    dataSet, features = createDataSet()
-    print("最优特征索引值:" + str(chooseBestFeatureToSplit(dataSet)))
+    dataSet, labels = createDataSet()
+    featLabels = []
+    myTree = createTree(dataSet, labels, featLabels)
+    print(myTree)
