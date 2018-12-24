c1a5b6067706a2de922bd0629c632f9c97c30891
LeungKwongWai <grant@localhost.localdomain>
2018-12-24 19:51:03 +0800

    12/24

--- E-mail_Classifier.py
+++ E-mail_Classifier.py
@@ -17,6 +17,7 @@ def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0] * len(vocabList)
     for word in inputSet:
         if word in vocabList:
+            # 如果词条存在于词汇表中，则置1
             returnVec[vocabList.index(word)] = 1
         else : print("the word: %s is not in my Vocabulary!" % word)
     return returnVec
@@ -38,6 +39,58 @@ def bagOfWords2VecMN(vocabList, inputSet):
     return returnVec
 
 """
+函数说明:朴素贝叶斯分类器训练函数
+ 
+Parameters:
+    trainMatrix - 训练文档矩阵，即setOfWords2Vec返回的returnVec构成的矩阵
+    trainCategory - 训练类别标签向量，即loadDataSet返回的classVec
+Returns:
+    p0Vect - 非侮辱类的条件概率数组
+    p1Vect - 侮辱类的条件概率数组
+    pAbusive - 文档属于侮辱类的概率
+"""
+def trainNBO(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    # 文档属于侮辱类的概率
+    pAbusive = sum(trainCategory)/float(numTrainDocs)
+    p0Num = np.ones(numWords) 
+    p1Num = np.ones(numWords)
+    p0Denom = 2.0
+    p1Denom = 2.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    p1Vect = np.log(p1Num/p1Denom)
+    p0Vect = np.log(p0Num/p0Denom)
+    return p0Vect, p1Vect, pAbusive
+
+"""
+函数说明:朴素贝叶斯分类器分类函数
+ 
+Parameters:
+    vec2Classify - 待分类的词条数组
+    p0Vec - 非侮辱类的条件概率数组
+    p1Vec -侮辱类的条件概率数组
+    pClass1 - 文档属于侮辱类的概率
+Returns:
+    0 - 属于非侮辱类
+    1 - 属于侮辱类
+"""
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    # 对应元素相乘。logA * B = logA + logB，所以这里加上log(pClass1)
+    p1 = sum(vec2Classify * p1Vec) + np.log(pClass1)
+    p0 = sum(vec2Classify * p0Vec) + np.log(1.0 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+"""
 函数说明:接收一个大字符串并将其解析为字符串列表
  
 Parameters:
@@ -57,23 +110,57 @@ Parameters:
 Returns:
     vocabSet - 返回不重复的词条列表，也就是词汇表
 """
-def createVocaBList(dataSet):
+def createVocabList(dataSet):
     # 创建一个空的不重复集合
     vocabSet = set([])
     for document in dataSet:
         vocabSet = vocabSet | set(document)
     return list(vocabSet)
 
-
-if __name__ == '__main__':
+"""
+函数说明:测试朴素贝叶斯分类器
+ 
+Parameters:
+    无
+Returns:
+    无
+"""
+def spamTest():
     docList = []
     classList = []
-    for i in range(1,26):
-        wordList = textParse(open("email/spam/%d.txt" %i, 'r', encoding='ISO-8859-1').read())
+    fullText = []
+    for i in range(1, 26):
+        wordList = textParse(open('email/spam/%d.txt' % i, 'r', encoding='ISO-8859-1').read())
         docList.append(wordList)
+        fullText.append(wordList)
+        # 标记垃圾邮件为1
         classList.append(1)
-        wordList = textParse(open("email/ham/%d.txt" %i, 'r', encoding='ISO-8859-1').read())
+        wordList = textParse(open('email/ham/%d.txt' % i, 'r', encoding='ISO-8859-1').read())
         docList.append(wordList)
+        fullText.append(wordList)
+        # 标记非垃圾邮件为0
         classList.append(0)
-    vocabList = createVocaBList(docList)
-    print(vocabList)
+    vocabList = createVocabList(docList)
+    trainingSet = list(range(50))
+    testSet = []
+    for i in range(10):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+    trainMat = []
+    trainClasses = []
+    for docIndex in trainingSet:
+        trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])  
+    # 训练朴素贝叶斯模型
+    p0V, p1V, pSpam = trainNBO(np.array(trainMat), np.array(trainClasses))
+    errorCount = 0    
+    for docIndex in testSet:
+        wordVector = setOfWords2Vec(vocabList, docList[docIndex])
+        if classifyNB(np.array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+            print("分类错误的测试集：", docList[docIndex])
+    print('错误率：%.2f%%' % (float(errorCount) / len(testSet) * 100))
+
+if __name__ == '__main__':
+    spamTest()
