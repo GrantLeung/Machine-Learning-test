07bd85d43585522f8b103cc17176718604aafd4e
LeungKwongWai <grant@localhost.localdomain>
2019-01-07 20:04:07 +0800

    2018-01-07

--- Logistic.py
+++ Logistic.py
@@ -1,14 +1,76 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+函数说明:梯度上升算法测试函数
+ 
+求函数f(x) = -x^2 + 4x的极大值
+ 
+Parameters:
+    无
+Returns:
+    无
+"""
 def Gradient_Ascent_test():
     def f_prime(x_old):
         return -2 * x_old + 4
-    x_old = 1
+    x_old = -1
     x_new = 0
     alpha = 0.01
     presision = 0.00000001
     while abs(x_new - x_old) > presision:
         x_old = x_new
         x_new = x_old + alpha * f_prime(x_old)
-        print(x_new)
+    print(x_new)
+
+"""
+函数说明:加载数据
+ 
+Parameters:
+    无
+Returns:
+    dataMat - 数据列表
+    labelMat - 标签列表
+Author:
+    Jack Cui
+"""
+def loadDataSet():
+    dataMat = []
+    labelMat = []
+    fr = open('./Logistic/testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    fr.close()
+    return dataMat, labelMat
+
+"""
+函数说明:绘制数据集
+ 
+Parameters:
+    无
+Returns:
+    无
+"""
+def plotDataSet():
+    dataMat, labelMat = loadDataSet()
+    dataArr = np.array(dataMat)
+    n = np.shape(dataMat)[0]
+    xcord1 = [];ycord1 = []
+    xcord2 = [];ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])
+        else:
+            xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)                                            #添加subplot
+    ax.scatter(xcord1, ycord1, s = 20, c = 'red', marker = 's',alpha=.5)#绘制正样本
+    ax.scatter(xcord2, ycord2, s = 20, c = 'green',alpha=.5)            #绘制负样本
+    plt.title('DataSet')                                                #绘制title
+    plt.xlabel('x'); plt.ylabel('y')                                    #绘制label
+    plt.show()    
 
 if __name__ == '__main__':
-    Gradient_Ascent_test()
+    plotDataSet()
--- split_sentence.py
+++ split_sentence.py
@@ -1,6 +1,9 @@
+from sklearn.naive_bayes import MultinomialNB
+import matplotlib.pyplot as plt
 import os
-import jieba
 import random
+import jieba
+ 
 
 """
 函数说明:中文文本处理
@@ -31,7 +34,7 @@ def TextProcessing(folder_path, test_size = 0.2):
         for file in files:
             if j > 100:
                 break
-            with open(os.path.join(new_folder_path, file), 'r', encoding = 'utf-') as f:
+            with open(os.path.join(new_folder_path, file), 'r', encoding = 'utf-8') as f:
                 raw = f.read()
             word_cut = jieba.cut(raw, cut_all = False)
             word_list = list(word_cut)
@@ -40,30 +43,126 @@ def TextProcessing(folder_path, test_size = 0.2):
             class_list.append(folder)
             j += 1
 
-        data_class_list = list(zip(data_list, class_list))
-        random.shuffle(data_class_list)
-        index = int(len(data_class_list) * test_size) + 1
-        train_list = data_class_list[index:]
-        test_list = data_class_list[:index]
-        train_data_list, train_class_list  =zip(*train_list)
-        test_data_list, test_class_list = zip(*test_list)
+    data_class_list = list(zip(data_list, class_list))
+    random.shuffle(data_class_list)
+    index = int(len(data_class_list) * test_size) + 1
+    train_list = data_class_list[index:]
+    test_list = data_class_list[:index]
+    train_data_list, train_class_list  =zip(*train_list)
+    test_data_list, test_class_list = zip(*test_list)
 
-        all_words_dict = {}
-        for word_list in train_data_list:
-            for word in word_list:
-                if word in all_words_dict.keys():
-                    all_words_dict[word] += 1
-                else:
-                    all_words_dict[word] = 1
+    all_words_dict = {}
+    for word_list in train_data_list:
+        for word in word_list:
+            if word in all_words_dict.keys():
+                all_words_dict[word] += 1
+            else:
+                all_words_dict[word] = 1
         
         #根据键的值倒序排序
 
-        all_words_tuple_list = sorted(all_words_dict.items(), key = lambda f:f[1], reverse = True)
-        all_words_list, all_words_nums = zip(*all_words_tuple_list)
-        all_words_list = list(all_words_list)
-        return all_words_list, train_data_list, test_data_list, train_class_list, test_class_list
+    all_words_tuple_list = sorted(all_words_dict.items(), key = lambda f:f[1], reverse = True)
+    all_words_list, all_words_nums = zip(*all_words_tuple_list)
+    all_words_list = list(all_words_list)
+    return all_words_list, train_data_list, test_data_list, train_class_list, test_class_list
+
+"""
+函数说明:读取文件里的内容，并去重
+ 
+Parameters:
+    words_file - 文件路径
+Returns:
+    words_set - 读取的内容的set集合
+"""
+def MakeWordsSet(words_file):
+    words_set = set()
+    with open(words_file, 'r', encoding = 'utf-8') as f:
+        for line in f.readlines():
+            word = line.strip()
+            if len(word) > 0:
+                words_set.add(word)
+    return words_set
+
+"""
+函数说明:根据feature_words将文本向量化
+ 
+Parameters:
+    train_data_list - 训练集
+    test_data_list - 测试集
+    feature_words - 特征集
+Returns:
+    train_feature_list - 训练集向量化列表
+    test_feature_list - 测试集向量化列表
+"""
+def TextFeatures(train_data_list, test_data_list, feature_words):
+    def text_features(text, feature_words):
+        text_words = set(text)
+        features = [1 if word in text_words else 0 for word in feature_words]
+        return features
+    train_feature_list = [text_features(text, feature_words) for text in train_data_list]
+    test_feature_list = [text_features(text, feature_words) for text in test_data_list]
+    return train_feature_list, test_feature_list
+
+"""
+函数说明:文本特征选取
+ 
+Parameters:
+    all_words_list - 训练集所有文本列表
+    deleteN - 删除词频最高的deleteN个词
+    stopwords_set - 指定的结束语
+Returns:
+    feature_words - 特征集
+"""
+def words_dict(all_words_list, deleteN, stopwords_set = set()):
+    feature_words = []
+    n = 1
+    for t in range(deleteN, len(all_words_list), 1):
+        #feature_words的维度为1000
+        if n > 1000:
+            break
+        #如果这个词不是数字，并且不是指定的结束语，并且单词长度大于1小于5，那么这个词就可以作为特征词
+        if not all_words_list[t].isdigit() and all_words_list[t] not in stopwords_set and 1 < len(all_words_list) < 5:
+            feature_words.append(all_words_list[t])
+        n += 1
+    return feature_words
+
+"""
+函数说明:新闻分类器
+ 
+Parameters:
+    train_feature_list - 训练集向量化的特征文本
+    test_feature_list - 测试集向量化的特征文本
+    train_class_list - 训练集分类标签
+    test_class_list - 测试集分类标签
+Returns:
+    test_accuracy - 分类器精度
+"""
+def TextClassifier(train_feature_list, test_feature_list, train_class_list, test_class_list):
+    classifier = MultinomialNB().fit(train_feature_list, train_class_list)
+    test_accuracy = classifier.score(test_feature_list, test_class_list)
+    return test_accuracy
 
 if __name__ == '__main__':
-    folder_path = 'SogouC/Sample'
+    #文本预处理
+    folder_path = './SogouC/Sample'                #训练集存放地址
     all_words_list, train_data_list, test_data_list, train_class_list, test_class_list = TextProcessing(folder_path, test_size=0.2)
-    print(all_words_list)
+
+    # 生成stopwords_set
+    stopwords_file = './SogouC/stopwords_cn.txt'
+    stopwords_set = MakeWordsSet(stopwords_file)
+
+
+    test_accuracy_list = []
+    deleteNs = range(0, 1000, 20)                #0 20 40 60 ... 980
+    for deleteN in deleteNs:
+        feature_words = words_dict(all_words_list, deleteN, stopwords_set)
+        train_feature_list, test_feature_list = TextFeatures(train_data_list, test_data_list, feature_words)
+        test_accuracy = TextClassifier(train_feature_list, test_feature_list, train_class_list, test_class_list)
+        test_accuracy_list.append(test_accuracy)
+
+    plt.figure()
+    plt.plot(deleteNs, test_accuracy_list)
+    plt.title('Relationship of deleteNs and test_accuracy')
+    plt.xlabel('deleteNs')
+    plt.ylabel('test_accuracy')
+    plt.show()
